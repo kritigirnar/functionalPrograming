f2639abdf4c8be3511be8a7cfbb8d30442aeacb6
kriti <kritigirnar@gmail.com>
2022-10-08 16:47:01 +0530

    Functional prog and optional classes

--- src/programing/FP01Functional.java
+++ src/programing/FP01Functional.java
@@ -5,16 +5,41 @@ import java.util.List;
 public class FP01Functional {
 public static void main(String[] args) {
 	
-	printFunctionalList(List.of(2,4,43,21,34));
+	//printFunctionalList(List.of(2,4,43,21,34));
+	printFunctionalEvenList(List.of(2,4,43,21,34));
+
 }
 
+
+
+/*
 private static void printNumbers(int num)
 {
 	System.out.println(num);
-	
-}
+	 
+}*/
+/*
 private static void printFunctionalList(List<Integer> numbers) {
 
-	numbers.stream().forEach(FP01Functional::printNumbers);
+	//numbers.stream().forEach(FP01Functional::printNumbers);
+	numbers.stream().forEach(System.out::println);
+}*/
+
+public static boolean evenNum(int num)
+{
+	return num %2==0;
 }
+private static void printFunctionalEvenList(List<Integer> numbers) {
+
+	//numbers.stream().filter(FP01Functional::evenNum).forEach(System.out::println);
+	/*numbers.stream().filter(
+			number->number%2==0
+			).forEach(System.out::println);
+*/
+	
+	numbers.stream().filter(a->a%2!=0).forEach(System.out::println);
+}
+
+	
+	
 }
--- src/programing/FP01Structured.java
+++ src/programing/FP01Structured.java
@@ -5,9 +5,10 @@ import java.util.List;
 public class FP01Structured {
 public static void main(String[] args) {
 	
-	printStructuredList(List.of(2,4,43,21,34));
+	//printStructuredList(List.of(2,4,43,21,34));
+	printStructuredEvenList(List.of(2,4,43,21,34));
 }
-
+/*
 private static void printStructuredList(List<Integer> numbers) {
 
 	for(int printOneByOne : numbers)
@@ -15,4 +16,20 @@ private static void printStructuredList(List<Integer> numbers) {
 		System.out.println(printOneByOne);
 	}
 }
+*/
+
+private static void printStructuredEvenList(List<Integer> listOfNumbers) {
+
+	for(int number: listOfNumbers){
+		
+		if(number%2==0)
+		{
+			System.out.println(number);
+		}
+	}
+}
+
+
+
+
 }
